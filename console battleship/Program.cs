cc41c106fa166caf327d2fc748c4884596574547
itsbaptiste <baptiste.manusset@gmail.com>
2019-12-20 16:34:56 +0100

    drop the mic

--- console battleship/Program.cs
+++ console battleship/Program.cs
@@ -5,7 +5,20 @@
         private static void Main(string[] args)
         {
             // C'est un peut vide ici ?
-
+            /*         
+             
+       .`' ` * .
+      :  *  *|  :
+       ' |  || '
+        `|~'||'
+        v~v~v~v
+        !@!@!@!
+       _!_!_!_!_
+      |  ||    ||
+      |  ||   |||
+      }{{{{}}}{{{
+            
+             */
             Game game = new Game();
         }
     }
